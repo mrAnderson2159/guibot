a73140621283ca3edd169d63d4b40e59dd08703d
MacBook - Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-07-17 17:50:23 +0200

    Add listen_for_hotkeys_then method to KeyboardListener class
    This method allows to listen for a combination of keys in order to trigger a callback

--- src/keyboard_listener.py
+++ src/keyboard_listener.py
@@ -100,7 +100,60 @@ class KeyboardListener:
 
         return cls.safe_start_listener(on_press=on_press)
 
-if __name__ == "__main__":
+
+    @classmethod
+    def listen_for_hotkeys_then(cls, keys: list[Union[str, Key]], callback: callable, *args, **kwargs) -> Listener:
+        """
+        Listen for a specific hotkey (combination of keys) and trigger callback when all are pressed together.
+        """
+        def normalize(key: Union[str, Key]) -> str:
+            if isinstance(key, str):
+                return key
+            else:
+                return key.name if hasattr(key, 'name') else key.char
+
+        expected = set(map(normalize, keys))
+        pressed = set()
+
+        def on_press(pressed_key):
+            if not cls.check_for_esc(pressed_key):
+                return False
+
+            try:
+                pressed.add(normalize(pressed_key))
+            except AttributeError:
+                pass  # Ignore keys without .name or .char
+
+            if pressed == expected:
+                callback(*args, **kwargs)
+
+            return True
+
+        def on_release(released_key):
+            try:
+                pressed.discard(normalize(released_key))
+            except AttributeError:
+                pass
+
+        log_keys = " + ".join(expected)
+        logger.info(f"Listening for hotkey: {log_keys}. Press all to trigger callback. Press 'esc' to stop.")
+
+        return cls.safe_start_listener(on_press=on_press, on_release=on_release)
+
+
+def __blocking_loop():
+    while True:
+        try:
+            pass  # Keep the main thread alive
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+
+__test = 'hotkeys'  # Change to 'multithreading' to test multithreading functionality
+
+
+if __name__ == "__main__" and __test == 'multithreading':
     # Example for listening to a specific key
     def specific_key_callback():
         print("Specific key pressed!")
@@ -108,9 +161,13 @@ if __name__ == "__main__":
     KeyboardListener.listen_for_key_then('a', specific_key_callback)
     KeyboardListener.listen_for_key_then('b', specific_key_callback)
 
-    while True:
-        try:
-            pass  # Keep the main thread alive
-        except KeyboardInterrupt:
-            print("Exiting...")
-            break
+    __blocking_loop()
+
+if __name__ == "__main__" and __test == 'hotkeys':
+    # Example for listening to a hotkey
+    def hotkey_callback():
+        print("cmd + ctrl + 1 pressed!")
+
+    KeyboardListener.listen_for_hotkeys_then([Key.cmd, Key.ctrl, '1'], hotkey_callback)
+
+    __blocking_loop()
