e680b716657bb8aa4fb5d83f850975fe72fc9268
Valerio Molinari Microtech <there.is.no.spoon.2159@gmail.com>
2025-09-02 14:26:46 +0200

    Add point operations

--- src/point.py
+++ src/point.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class Point:
     """ Represents a point in 2D space with x and y coordinates."""
     def __init__(self, x: float, y: float):
@@ -30,3 +32,34 @@ class Point:
 
     def __repr__(self):
         return f"Point({{x: {self.x}, y: {self.y}}})"
+
+    def __add__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar: float):
+        if not isinstance(scalar, (int, float)):
+            return NotImplemented
+        return Point(self.x * scalar, self.y * scalar)
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __round__(self, ndigits=None) -> "Point":
+        return Point(round(self.x, ndigits), round(self.y, ndigits))
+
+    def scale(self, factor_x: float, factor_y: Optional[float] = None) -> "Point":
+        """ Scales the point by the given factors.
+        :param factor_x: The factor to scale the x-coordinate.
+        :param factor_y: The factor to scale the y-coordinate. If None, factor_y will be set to factor_x.
+        """
+        if factor_y is None:
+            factor_y = factor_x
+        return Point(self.x * factor_x, self.y * factor_y)
