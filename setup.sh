9f0a40b8a7bd52a3096f2db7ab73242fddb72bc3
Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2025-09-16 22:05:53 +0200

    Add linux setup settings

--- setup.sh
+++ setup.sh
@@ -17,6 +17,37 @@ cd "$(cd "$(dirname "$0")" && pwd)" || {
 }
 echo "OK"
 
+# Controllo dipendenze di sistema (solo Linux)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo -n "Controllo pacchetti di sistema richiesti... "
+
+    PY_VER=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+    DEV_PKG="python${PY_VER}-dev"
+
+    MISSING=""
+    for pkg in build-essential $DEV_PKG; do
+        dpkg -s "$pkg" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            MISSING="$MISSING $pkg"
+        fi
+    done
+
+    if [ -n "$MISSING" ]; then
+        echo "Pacchetti mancanti: $MISSING"
+        echo "Installazione in corso..."
+        sudo apt update && sudo apt install -y $MISSING
+        if [ $? -eq 0 ]; then
+            echo "OK"
+        else
+            echo "Errore nell'installazione dei pacchetti richiesti."
+            exit 1
+        fi
+    else
+        echo "OK"
+    fi
+fi
+
+
 # Creazione ambiente virtuale
 echo -n "Creazione ambiente virtuale... "
 if [ ! -d "venv" ]; then
@@ -64,3 +95,7 @@ else
     echo "Errore nell'impostazione del PYTHONPATH."
     exit 1
 fi
+
+
+echo -e "\nSetup completato. Ora attiva il venv locale con:\x1b[0m"
+echo -e "\t\x1b[36msource venv/bin/activate\n"
